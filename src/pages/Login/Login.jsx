032ff8a6de2b3416dab60f9e2f84458a6d9584c7
faisal-shohag <faisalshohagprog@gmail.com>
2024-07-02 12:58:20 +0600

    updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,24 @@
-import { Link } from "react-router-dom";
+
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
+    const {signIn} = useAuth()
+    const navigate = useNavigate()
+
+    const handleSignIn = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+        signIn(email, password)
+        .then(result => {
+            console.log(result.user)
+            navigate('/dashboard')
+        })
+        .catch(err => console.log(err))
+    }
     return (
         <body>
             <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
@@ -21,7 +39,7 @@ const Login = () => {
                                 alt="Office"
                             />
                         </div>
-                        <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                        <form onSubmit={handleSignIn} className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                             <div className="w-full">
                                 <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                                     Login
@@ -30,8 +48,9 @@ const Login = () => {
                                     <span className="text-gray-700 dark:text-gray-400">Phone</span>
                                     <input
                                         className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                        placeholder="01755232541"
-                                        type="number"
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
                                     />
                                 </label>
                                 <label className="block mt-4 text-sm">
@@ -40,15 +59,15 @@ const Login = () => {
                                         className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                                         placeholder="***************"
                                         type="password"
+                                        name="password"
                                     />
                                 </label>
 
 
-                                <Link
-                                    className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                                    to="" >
+                                <button
+                                    className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                     Log in
-                                </Link>
+                                </button>
 
                                 <hr className="my-8" />
 
@@ -68,7 +87,7 @@ const Login = () => {
                                     </Link>
                                 </p>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
