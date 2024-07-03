107a098def7bf5f22f9a26ca92b4a064745c450b
faisal-shohag <faisalshohagprog@gmail.com>
2024-07-03 12:03:39 +0600

    update

--- src/pages/Dashboard/Courses.jsx
+++ src/pages/Dashboard/Courses.jsx
@@ -7,6 +7,7 @@ import useAuth from '../../hooks/useAuth';
 const Courses = () => {
     const {user} = useAuth()
     const {data} = user
+    const {courses} = data
     console.log(data);
     return (
         <>
@@ -17,7 +18,8 @@ const Courses = () => {
             </h2>
             
             <div className="grid gap-10 mb-8 md:grid-cols-2">
-                <div className="max-w-md overflow-hidden rounded-lg bg-white shadow">
+                {courses.map((course, index) => (
+                    <div key={index} className="max-w-md overflow-hidden rounded-lg bg-white shadow">
                     <img
                         src="https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
                         className="aspect-video w-full object-cover"
@@ -25,19 +27,20 @@ const Courses = () => {
                     />
                     <div className="p-4">
                         <div className="flex gap-2 ">
-                            <span className="inline-flex items-center gap-1 rounded-full bg-blue-500 px-2 py-1 text-xs font-semibold text-white mr-3"> ব্যাচ ২ </span>
-                            <span className="inline-flex items-center gap-1 rounded-full bg-blue-500 px-2 py-1 text-xs font-semibold text-white"> অনগোয়িং </span>
+                            <span className="inline-flex items-center gap-1 rounded-full bg-blue-500 px-2 py-1 text-xs font-semibold text-white mr-3"> ব্যাচ {course.course.batch} </span>
+                            <span className="inline-flex items-center gap-1 rounded-full bg-blue-500 px-2 py-1 text-xs font-semibold text-white"> {course.course.status} </span>
                         </div>
                         <div className="mt-5">
-                            <h3 className="text-xl font-medium text-gray-900 mt-5">ওয়েব ডিজাইন (Web Design & Development)</h3>
+                            <h3 className="text-xl font-medium text-gray-900 mt-5">{course.course.title}</h3>
                         </div>
                         <div>
-                            <Link to='/dashboard/course-details' className="hover:bg-slate-500 w-full flex items-center justify-center bg-black text-white py-2 rounded-md mt-4">
+                            <Link to={`/dashboard/course-details/${course.course.id}`} className="hover:bg-slate-500 w-full flex items-center justify-center bg-black text-white py-2 rounded-md mt-4">
                                 এগিয়ে যাই <ArrowRightFromLine className="ml-3"/>
                             </Link>
                         </div>
                     </div>
                 </div>
+                ))}
 
             </div>
 
