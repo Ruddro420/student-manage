25ca898c1a8dcb710e699b0f982f022cf17e6096
touhidul-islam-html <ti360109@gmail.com>
2025-05-05 01:46:07 +0600

    update

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -109,7 +109,7 @@ const Register = () => {
                     className="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
                   >
                     <option>Select Course</option>
-                    {courses.map((course) => (
+                    {courses?.map((course) => (
                       <option key={course.id} value={course.course_name}>
                         {course.course_name}-{course.batch_no}
                       </option>
